6801ec6b9a2deaaacdda9bfe6f2b0b142756571c
AndreaPanzeriDev <andpanz@libero.it>
2022-10-19 18:27:06 +0200

    input funzionanti

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,33 @@
 
 
-const Kmprice = 0.21;
 
+
+
+function PriceCalc(){
+
+    const Kmprice = 0.21;
+    var RidePrice;
+    let Km = document.getElementById("kmdafare").value;
+    console.log(Km);
+    let age = document.getElementById("clientage").value;
+    console.log(age);
+
+    switch(age){
+        case age < 18:
+            console.log("minorenni");
+            RidePrice = (Km * Kmprice) * 0.2;
+            console.log("costo corsa: ", RidePrice);
+            break;
+        case age > 65:
+            console.log("anziani");
+            RidePrice = (Km * Kmprice) * 0.4;
+            console.log("costo corsa: ", RidePrice);
+            break;
+        
+        default:
+            RidePrice = Km * Kmprice;
+            console.log("costo corsa: ", RidePrice);
+            
+    }
+
+}
